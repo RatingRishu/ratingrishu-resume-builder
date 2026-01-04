9bc4bcfc1eff095df5a3ae18e1a06f545e9941f2
RatingRishu <rairishabh5001@gmail.com>
2026-01-04 19:45:39 +0530

    fix gh-pages workflow for vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import { HashRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  
-  base: "/ratingrishu-resume-builder/", // ✅ MUST be here
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [
-    react(),
-    // mode === "development" && componentTagger(),
-  ].filter(Boolean),
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: "/ratingrishu-resume-builder/",
+  plugins: [react()],
+});
